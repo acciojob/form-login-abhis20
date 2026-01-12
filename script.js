2cc7702d49da9d199897208940ac5b912feae695
Abhishek <135864521+abhis20@users.noreply.github.com>
2026-01-12 12:03:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    event.preventDefault(); 
 
+    let firstName = document.querySelector("input[name=fname]").value.trim();
+    let lastName = document.querySelector("input[name=lname]").value.trim();
+
+    if(firstName && lastName){
+		alert(firstName + " " + lastName);
+	}
+	else{
+		alert("Enter Values");
+	}
+	
 }
